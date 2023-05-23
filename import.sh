8fee90917a891a494f07140801dec8337e4c3611
Daniel Proctor <dan@danielproctor.dev>
2023-05-23 22:15:17 +0100

    add sunshine

--- import.sh
+++ import.sh
@@ -10,3 +10,7 @@ flatpak override --user com.heroicgameslauncher.hgl --filesystem=xdg-config/Mang
 flatpak override --user net.lutris.Lutris --filesystem=xdg-config/MangoHud:ro
 
 flatpak override --user --filesystem=~/.local/share/applications:create --filesystem=~/.local/share/icons:create com.google.Chrome
+
+# permission to launch applications and mouse input (input also requires udev rule: https://github.com/LizardByte/Sunshine/blob/master/src_assets/linux/misc/85-sunshine.rules)
+flatpak override --user --talk-name=org.freedesktop.Flatpak dev.lizardbyte.app.Sunshine
+usermod -a -G input $USER
