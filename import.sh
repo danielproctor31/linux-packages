872d8a8842e69a562fdab8a7e0cc2f270b582589
Daniel <10681172+danielproctor31@users.noreply.github.com>
2023-05-25 19:58:57 +0100

    Update import.sh

--- import.sh
+++ import.sh
@@ -5,12 +5,12 @@ flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flath
 cat packages.txt | xargs flatpak install flathub --user --noninteractive
 
 # set permissions
-flatpak override --user com.valvesoftware.Steam --filesystem=xdg-config/MangoHud:ro
-flatpak override --user com.heroicgameslauncher.hgl --filesystem=xdg-config/MangoHud:ro
-flatpak override --user net.lutris.Lutris --filesystem=xdg-config/MangoHud:ro
+flatpak override com.valvesoftware.Steam --filesystem=xdg-config/MangoHud:ro
+flatpak override com.heroicgameslauncher.hgl --filesystem=xdg-config/MangoHud:ro
+flatpak override net.lutris.Lutris --filesystem=xdg-config/MangoHud:ro
 
-flatpak override --user --filesystem=~/.local/share/applications:create --filesystem=~/.local/share/icons:create com.google.Chrome
+flatpak override --filesystem=~/.local/share/applications:create --filesystem=~/.local/share/icons:create com.google.Chrome
 
 # permission to launch applications and mouse input (input also requires udev rule: https://github.com/LizardByte/Sunshine/blob/master/src_assets/linux/misc/85-sunshine.rules)
-flatpak override --user --talk-name=org.freedesktop.Flatpak dev.lizardbyte.app.Sunshine
+flatpak override --talk-name=org.freedesktop.Flatpak dev.lizardbyte.app.Sunshine
 usermod -a -G input $USER
